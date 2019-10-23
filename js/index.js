b7405ffa6efc7e9d8ebd956893eb376d0a9ef8ea
Your Name <you@example.com>
2019-10-23 14:44:37 +0800

    once

--- js/index.js
+++ js/index.js
@@ -4,17 +4,17 @@ $(function(){
         let clientW =$(window).width();
         // 设置临界点
         let isShowBigImg = clientW >= 900;
+        console.log(isShowBigImg);
         // 获取所有item
         let $allItem = $('#R_carouse_1 .carousel-item');
-        // console.log(allItem);
+        
         //遍历
         $allItem.each((index,item) => {
             // console.log(item);
             // 取出图片路径
             let src = isShowBigImg ? $(item).data('lg-img'):$(item).data('sm-img');
-            
             let imgUrl = `url(${src})`;
-            console.log(imgUrl);
+            // console.log(imgUrl);
             // 设置背景
             $(item).css({
                 backgroundImage:imgUrl,
@@ -23,9 +23,9 @@ $(function(){
             if(!isShowBigImg){
                 let imgEle = `<img sec = "${src}">`;
                 $(item).empty().append(imgEle);
+            }else{
+                $(item).empty();
             }
-            //  
         })
-
     }).trigger('resize');
 })
