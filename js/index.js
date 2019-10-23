6e13ca62c808f0b6d0220cfa75c8eb04793ffca4
ShuoyuGong <2788696894@qq.com>
2019-10-23 11:12:40 +0800

    once

--- js/index.js
+++ js/index.js
@@ -5,11 +5,20 @@ $(function(){
         // 设置临界点
         let isShowBigImg = clientW >= 900;
         // 获取所有item
-        let allItem = $('#R_carouse_1 .carousel-inner .carousel-item');
+        let $allItem = $('#R_carouse_1 .carousel-inner .carousel-item');
         // console.log(allItem);
         //遍历
-        allItem.each((index,item) => {
-            console.log(item);
+        $allItem.each((index,item) => {
+            // console.log(item);
+            // 取出图片路径
+            let src = isShowBigImg ? $(item).data('lg-img'):$(item).data('sm-img');
+            
+            let imgUrl = `url(${src})`;
+            console.log(imgUrl);
+            // 设置背景
+            $(item).css({
+                backgroundImage:imgUrl,
+            })
         })
 
     }).trigger('resize');
